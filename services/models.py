427e9450caa0c596a7809e074f08c3945c0b19f9
pasiac <dawidpawlowski16@gmail.com>
2020-11-19 18:39:46 +0100

    Fajniejsza struktura projektu

--- services/models.py
+++ services/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Service(models.Model):
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=1024)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
--- services/views.py
+++ services/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView)
 
-# Create your views here.
+from services.forms import ServiceForm
+from services.models import Service
+
+
+class ServiceListView(ListView):
+    model = Service
+    paginate_by = 10
+
+
+class ServiceCreateView(CreateView):
+    model = Service
+    form_class = ServiceForm
+    success_url = reverse_lazy("expense_list")
