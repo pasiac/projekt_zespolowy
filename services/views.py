7f735a414ba057ac45454e4e695657d4ce147357
pasiac <dawidpawlowski16@gmail.com>
2020-12-03 14:26:08 +0100

    Api

--- services/views.py
+++ services/views.py
@@ -12,13 +12,31 @@ class ServiceListView(LoginRequiredMixin, ListView):
     model = Service
     paginate_by = 10
 
+class ServiceDetailView(LoginRequiredMixin, ListView):
+    model = Service
 
 class ServiceCreateView(LoginRequiredMixin, CreateView):
     model = Service
     form_class = ServiceForm
     success_url = reverse_lazy("service_list")
+    extra_context = {"header": "Dodawanie usługi"}
+
 
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.created_by = self.request.user
         return super().form_valid(form)
+
+class ServiceDeleteView(LoginRequiredMixin, DeleteView):
+    model = Service
+    success_url = reverse_lazy("service_list")
+    
+    # TODO: Create confirmation in js
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+class ServiceUpdateView(LoginRequiredMixin, UpdateView):
+    model = Service
+    form_class = ServiceForm
+    success_url = reverse_lazy("service_list")
+    extra_context = {"header": "Edycja usługi"}
