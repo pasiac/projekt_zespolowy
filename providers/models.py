e207450c353a40f546935bfe5130d0d6e48468b8
pasiac <dawidpawlowski16@gmail.com>
2020-12-09 18:22:34 +0100

    Aplikacja providers

---
+++ providers/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
