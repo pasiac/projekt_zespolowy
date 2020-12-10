59c8ce200dcdfa355d9928281f4d0c86ae1701bf
pasiac <dawidpawlowski16@gmail.com>
2020-12-10 01:25:11 +0100

    Dodane zakomentowane sygnaly

--- accounts/models.py
+++ accounts/models.py
@@ -13,4 +13,14 @@ class CreditCard(models.Model):
 
 class CustomerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
+# Sygnały w ktorych bedziemy dostawac jaki typ profilu wybral rejestrujacy sie i w zaleznosci od tego
+# tworzyc odpowiedni profil biznesowy/konsumencki 
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
