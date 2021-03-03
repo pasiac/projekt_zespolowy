97f775696744df064fbad092d7d05cd25fefd7a9
Dawid Pawlowski <ext.dawid.pawlowski@fabrity.pl>
2021-03-03 18:08:17 +0100

    Delete credentials

--- settings.py
+++ settings.py
@@ -120,8 +120,7 @@ if instance == "prod":
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'uslugrezerwacja'
-EMAIL_HOST_PASSWORD = 'projektzespolowy1!'
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 #
